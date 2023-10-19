ee835ffcb270eaa37a55ec56424cf822e1be6977
cabersdev <caberlotto.giovanni@gmail.com>
2023-10-19 10:00:07 +0200

    Implement the login functions

--- main.c
+++ main.c
@@ -8,10 +8,21 @@ int main(int argc, char *argv[])
 {
   if (isatty(STDIN_FILENO)) {
     // shell is running in interactive mode
-    // display prompt and accept input from the user
+    // display prompt  
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    char username = *getlogin();
+    char hostname[1024];
+    gethostname(hostname, sizeof(hostname));
+    printf("%s@%s:%s\n", username, hostname, cwd);
+
+    // accept input from the user
+    
+
 
   } else {
-
+    // shell is running in non-interactive mode
+    // execute commands from script or batch file
   }
 
   return 0;
